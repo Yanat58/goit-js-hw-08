9dfe1bd89f776dd6fab183307792f138da929736
Tetiana Karlova <tanyalyt80@gmail.com>
2023-02-28 22:26:06 +0200

    task-2.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(CURRENT_TIME_KEY, seconds);
+}
+
+player
+  .setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY))
+  .then(function (seconds) {
+    seconds = localStorage.getItem(CURRENT_TIME_KEY);
+  })
+  .catch(function (error) {
+    console.log('An error occurred while setting the time');
+  });
