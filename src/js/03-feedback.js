9772543418128da0f5b207596f72ee0e22e201aa
Tetiana Karlova <tanyalyt80@gmail.com>
2023-03-01 18:10:18 +0200

    fixed task-3.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,6 @@
 import throttle from 'lodash.throttle';
 
-const FORM__KEY = 'feedback-form-state';
+const FORM_KEY = 'feedback-form-state';
 
 const formRef = document.querySelector('.feedback-form');
 const inputRef = document.querySelector('input');
@@ -16,17 +16,24 @@ const formData = {};
 function onFormSubmit(e) {
   e.preventDefault();
 
+  if (inputRef.value == '' || textareaRef.value == '') {
+    return alert('Please fill in all the fields!');
+  }
+  console.log({
+    email: inputRef.value,
+    message: textareaRef.value,
+  });
   e.target.reset();
-  localStorage.removeItem(FORM__KEY);
+  localStorage.removeItem(FORM_KEY);
 }
 
 function onFormInput(e) {
   formData[e.target.name] = e.target.value;
-  localStorage.setItem(FORM__KEY, JSON.stringify(formData));
+  localStorage.setItem(FORM_KEY, JSON.stringify(formData));
 }
 
 function populateForm() {
-  const savedData = JSON.parse(localStorage.getItem(FORM__KEY));
+  const savedData = JSON.parse(localStorage.getItem(FORM_KEY));
   if (savedData) {
     inputRef.value = savedData.email;
     textareaRef.value = savedData.message;
