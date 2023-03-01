e507981268a4f7e8d82730892f87a8c3b37d23bb
Tetiana Karlova <tanyalyt80@gmail.com>
2023-03-01 14:26:18 +0200

    task-3.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,7 +9,8 @@ const player = new Player(iframe);
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
-  localStorage.setItem(CURRENT_TIME_KEY, seconds);
+  console.log(localStorage.setItem(CURRENT_TIME_KEY, seconds));
+  // localStorage.setItem(CURRENT_TIME_KEY, seconds);
 }
 
 player
@@ -18,5 +19,13 @@ player
     seconds = localStorage.getItem(CURRENT_TIME_KEY);
   })
   .catch(function (error) {
-    console.log('An error occurred while setting the time');
+    switch (error.name) {
+      case 'RangeError':
+        seconds = 0 || seconds > player.duration;
+        break;
+
+      default:
+        console.log('An error occurred while setting the time');
+      // some other error occurred
+    }
   });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const inputRef = document.querySelector('input');
+const textareaRef = document.querySelector('textarea');
+const buttonRef = document.querySelector('button');
+
+console.log(inputRef.value);
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onFormInput), 500);
+
+populateForm();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.target.reset();
+  localStorage.removeItem('key');
+  localStorage.removeItem('key1');
+}
+
+function onFormInput(e) {
+  if (inputRef.value === e.target.value) {
+    const email = e.target.value;
+    localStorage.setItem('key', email);
+  } else if (textareaRef.value) {
+    const message = e.target.value;
+    localStorage.setItem('key1', message);
+  }
+}
+
+function populateForm() {
+  const savedEmail = localStorage.getItem('key');
+  const savedMessage = localStorage.getItem('key1');
+  if (savedEmail || savedMessage) {
+    inputRef.value = savedEmail;
+    textareaRef.value = savedMessage;
+  }
+}
